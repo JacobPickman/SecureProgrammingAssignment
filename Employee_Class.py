f503b99806de4fb26acd7b5df303df8a7917b420
JacobPickman <78869483+JacobPickman@users.noreply.github.com>
2022-12-03 14:07:43 -0600

    Finished Program, now we can secure it

--- Employee_Class.py
+++ Employee_Class.py
@@ -1,82 +1,76 @@
 from abc import ABC, abstractmethod
 
 class Employee(ABC):
-    
+
     def __init__(self, name, number, working):
-        self.name = name
-        self.number = number
-        self.working = working
-    
+        self.__name = name
+        self.__number = number
+        self.__working = working
+
     def getName(self):
         return self.__name
-    
+
     def getNum(self):
         return self.__number
-    
+
     def getWork(self):
         return self.__working
-    
+
     def setName(self, name):
         self.__name = name
-    
+
     def setNum(self, num):
         self.__number = num
-        
+
     def setWork(self, work):
         self.__working = work
-        
+
     @abstractmethod
     def getPay(self):
         pass
-    
+
     def __str__(self):
-        return str(self.__name) + "\t" + str(self.__num) + "\t" + str(self.__working)
-    
-    
-    
+        return "Name: "+ str(self.__name) + "\tID: " + str(self.__number) + "\tCurrently Working: " + str(self.__working)
+
+
+
 class StudentEmployee(Employee):
-    def __init__(self, name, number, working, hours, workstudy, rate):
-        self.name = name
-        self.number = number
-        self.working = working
-        self.hours = hours
-        self.workstudy = workstudy
-        self.rate = rate
+    def __init__(self, name, num, working, hours, workstudy, rate):
+        super().__init__(name, num, working)
+        self.__hours = hours
+        self.__workstudy = workstudy
+        self.__rate = rate
 
     def getPay(self):
         return self.__hours * self.__rate
-    
+
     def __str__(self):
-        return super()+ "\t" + self.__hours + "\t" + self.__workstudy + "\t" + self.__rate;
-    
+        return super(StudentEmployee, self).__str__() + "\tHours Worked: " + str(self.__hours) + "\tEnrolled Workstudy: " + str(self.__workstudy) +"\tPay Rate: " + str(self.__rate)
+
 
 
 class ClassifiedStaff(Employee):
     def __init__(self, name, num, working, sal, div):
-        self.__name = name
-        self.__num = num
-        self.__working = working
+        super().__init__(name, num, working)
         self.__sal = sal
         self.__div = div
-        
+
     def getPay(self):
-        return self.__sal * 2
-    
+        return int(self.__sal) * 2
+
     def __str__(self):
-        return super() +"\t" +  self.__sal +"\t" +  self.__div
-    
-    
+        return super(ClassifiedStaff, self).__str__() +"\tBiweekly Pay: " +  str(self.getPay())
+
+
 class Faculty(Employee):
     def __init__(self, name, num, working, sal, week, dept):
-        self.__name = name
-        self.__num = num
-        self.__working = working
+        super().__init__(name, num, working)
         self.__sal = sal
         self.__week = week
         self.__dept = dept
-        
+
     def getPay(self):
         return (sal / week) * 2
-    
+
     def __str__(self):
-        return super().__str__ + "\t" +  str(self.__sal) + "\t" + str(self.__week) + "\t" + str(self.__dept)
+        return super(Faculty, self).__str__() + "\tSalary: " + str(self.__sal) + "\tPay Period: " + str(self.__week) + " weeks\tDepartment: " + str(self.__dept)
--- Main.py
+++ Main.py
@@ -1,11 +1,26 @@
 from Employee_Class import Employee, StudentEmployee, ClassifiedStaff, Faculty
 
-file = open(input("Enter file name: "), "w")
+
+file = open(input("Enter file name: "), "r")
+
 
 numStu = input("Enter the number of students: ")
 numCS = input("Enter the number of Classified Staff: ")
 numFac = input("Enter the number of Faculty: ")
-numTot = numStu + numCS + numFac
 
-myemp = Faculty("Jim", 10, True, 10 ,2, "Sci")
-print(myemp)
+employees = []
+count = 0
+for line in file:
+    templine = line.split(",")
+    if count < int(numStu):
+        employees.append(StudentEmployee(templine[0], templine[1], templine[2], templine[3], templine[4], templine[5]))
+    elif count < int(numStu) + int(numCS) and count >= int(numStu):
+        employees.append(ClassifiedStaff(templine[0], templine[1], templine[2], templine[3], templine[4]))
+    else:
+        employees.append(Faculty(templine[0], templine[1], templine[2], templine[3], templine[4], templine[5]))
+    count+= 1
+
+print("\t\t\t\t\t\tPay Table\n")
+for entry in employees:
+    print()
+    print(entry, end = "")
