f98745b38188883170246ef8d88fb141262fccd9
JacobPickman <78869483+JacobPickman@users.noreply.github.com>
2022-12-03 14:54:18 -0600

    Finished testing StudentEmployee class
    Implemented tests to check the setters of Employee class and to check the StudentEmployee implementation of the abstract getPay() method. Also printed StudentEmployee object to see if it matches desired outcome

--- test_EmployeeClass.py
+++ test_EmployeeClass.py
@@ -2,7 +2,7 @@ from Employee_Class import *
 import unittest
 
 class TestEmployeeClasses(unittest.TestCase):
-    def testStudentEmployeeCreation(self):
+    def testStudentEmployee(self):
         test = StudentEmployee("Johnny", 10000, True, 40, True, 10)
         self.assertTrue(test.getName() == "Johnny")
         self.assertFalse(test.getName() == "Jimmy")
@@ -14,6 +14,30 @@ class TestEmployeeClasses(unittest.TestCase):
         
         self.assertTrue(test.getWork())
         self.assertFalse(test.getWork() == False)
+        
+        test.setName("Teddy")
+        self.assertTrue(test.getName() == "Teddy")
+        self.assertFalse(test.getName() == "Johnny")
+        
+        test.setNum(20000)
+        self.assertTrue(test.getNum() == 20000)
+        self.assertFalse(test.getNum() == 10000)
+        
+        test.setWork(False)
+        self.assertTrue(test.getWork() == False)
+        self.assertFalse(test.getWork() == True)
+        
+        self.assertTrue(test.getPay() == 400)
+        self.assertFalse(test.getPay() == 500)
+        self.assertFalse(test.getPay() == 0)
+        self.assertFalse(test.getPay() == 1)
+        
+        testLine = print(test)
+        #self.assertTrue(testLine == "Name: Teddy\tID: 20000\tCurrently Working: False\tHours Worked: 40\tEnrolled Workstudy: True\tPay Rate: 10")
+
+
+        
+        
         
         
 if __name__ == '__main__':
